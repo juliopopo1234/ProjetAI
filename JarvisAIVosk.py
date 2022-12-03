350765e432a745cd49511d798ee54cd6ea4a1d30
unknown <ikaratchi@gmail.com>
2022-12-03 21:28:21 +0100

    Test de Vosk avec le micro de PyAudio, chez moi ça marche(Dragonlord)

--- JarvisAIVosk.py
+++ JarvisAIVosk.py
@@ -1,7 +1,35 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
 
-model = Model("C:\Users\Dragonlord-PC3060\Documents\ProjetAIvosk\ProjetAI\vosk_speech_engine\model\vosk-model-fr-0.22")
+#config_AnaelBM = 
+config_Dragonlord = "C:/Users/Dragonlord-PC3060/Documents/ProjetAIvosk/ProjetAI/vosk_speech_engine/model/vosk-model-fr-0.22"
+
+model = Model(config_Dragonlord)
 recogniser = KaldiRecognizer(model, 16000)
 
 mic = pyaudio.PyAudio()
+stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
+
+def Listen():
+    print("")
+    print("Listening...")
+    print("")
+
+    while True:
+
+        data = stream.read(4096)
+
+        if recogniser.AcceptWaveform(data):
+            text = recogniser.Result()
+            p = text[14:-3]
+            print(f"Vous avez dit : {p}")
+
+            if len(p)>0:
+                return p
+            
+            else:
+                break
+
+while True :
+    Listen()
